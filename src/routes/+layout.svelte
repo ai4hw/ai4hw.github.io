e41ab56bf11ad1104351362b6648a5c02c6555a1
André Carvalho <andrefatela@icloud.com>
2026-09-18 12:14:40 +0200

    initial sveltekit setup

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import './layout.css';
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	<title>AI4HW | HiPEAC 2027</title>
+</svelte:head>
+{@render children()}
